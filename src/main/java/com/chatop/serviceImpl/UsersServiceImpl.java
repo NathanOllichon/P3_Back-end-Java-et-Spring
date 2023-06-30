5fa233b98a9e3bf4c02107af2fcacdb34f6ab7c5
NathanOllichon <ollichon.nathan@gmail.com>
2023-06-30 20:17:17 +0200

    use Bcrypt in place of Base64 encodage

--- src/main/java/com/chatop/serviceImpl/UsersServiceImpl.java
+++ src/main/java/com/chatop/serviceImpl/UsersServiceImpl.java
@@ -1,6 +1,7 @@
 package com.chatop.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import com.chatop.dto.UsersJsonDTO;
@@ -22,16 +23,28 @@ public class UsersServiceImpl implements UsersService {
     
     @Override
 	public void registerUser(UsersJsonDTO usersJsonDTO) {
-    	String encodedString = java.util.Base64.getEncoder().encodeToString(usersJsonDTO.getPassword().getBytes());
-    	Users usersCreated = new Users(null, usersJsonDTO.getEmail(), usersJsonDTO.getName(), encodedString, null, null);
+    	//String salt = BCrypt.gensalt();
+    	//String password_hash = BCrypt.hashpw(usersJsonDTO.getPassword(), salt);
+    	
+    	String password_hash = BCrypt.hashpw(usersJsonDTO.getPassword(), BCrypt.gensalt());
+
+    	
+//    	String encodedString = java.util.Base64.getEncoder().encodeToString(usersJsonDTO.getPassword().getBytes());
+//    	Users usersCreated = new Users(null, usersJsonDTO.getEmail(), usersJsonDTO.getName(), encodedString, null, null);
+    	Users usersCreated = new Users(null, usersJsonDTO.getEmail(), usersJsonDTO.getName(), password_hash, null, null);
+    	
     	usersRepository.save(usersCreated);
     }
     
     @Override
 	public Users isLoginValid(String mail, String password) {
     	Users user = usersRepository.findByEmail(mail);
-    	String encodedString = java.util.Base64.getEncoder().encodeToString(password.getBytes());
-    	if(user.getPassword().equals(encodedString)) {
+    	//String encodedString = java.util.Base64.getEncoder().encodeToString(password.getBytes());
+
+    	//String password_hash = BCrypt.hashpw(password, BCrypt.gensalt());
+
+//    	if(user.getPassword().equals(password_hash)) {
+    	if(BCrypt.checkpw(password, user.getPassword())) {
     		return user;
     	}
     	return null;
